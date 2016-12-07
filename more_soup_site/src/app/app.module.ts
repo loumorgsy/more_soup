e49131f28dd301a406721a7943f658c5beb06596
Louis Morgan <louismorgandesign@gmail.com>
2016-12-07 19:53:34 +0000

    routes added for links, works fine - completed about page and set up routes so a blank url leads to the home page

--- more_soup_site/src/app/app.module.ts
+++ more_soup_site/src/app/app.module.ts
@@ -3,21 +3,39 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about.component';
 import { ContactComponent } from './contact.component';
 import { WorkComponent } from './work.component';
+import { HomeComponent } from './home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
+  {
+   path: '',
+   redirectTo: '/home',
+   pathMatch: 'full'
+ },
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent, AboutComponent, ContactComponent, WorkComponent
+    AppComponent, HomeComponent, AboutComponent, ContactComponent, WorkComponent
   ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
